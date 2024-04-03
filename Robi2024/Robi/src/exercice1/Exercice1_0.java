64dc0c4a02cd2a0018215e11d238918308bf2c8b
Erwan <erwan.lebras27@gmail.com>
2024-04-03 12:12:36 +0200

    first exercice

--- Robi2024/Robi/src/exercice1/Exercice1_0.java
+++ Robi2024/Robi/src/exercice1/Exercice1_0.java
@@ -1,21 +1,57 @@
 package exercice1;
 
 import java.awt.Dimension;
+import java.awt.Point;
+
 import graphicLayer.GRect;
 import graphicLayer.GSpace;
 
-
 public class Exercice1_0 {
-	GSpace space = new GSpace("Exercice 1", new Dimension(200, 150));
-	GRect robi = new GRect();
+    GSpace space;
+    GRect robi;
+    int speed = 1;
 
-	public Exercice1_0() {
-		space.addElement(robi);
-		space.open();
-	}
+    public Exercice1_0() {
+        this.space = new GSpace("Exercice 1", new Dimension(400, 400));
+        this.robi = new GRect();
+        this.space.addElement(robi);
+        this.space.open();
+    }
 
-	public static void main(String[] args) {
-		new Exercice1_0();
-	}
+    public void tourFenetre() {
+    	while(true) {
+    		int x = this.robi.getPosition().x;
+            int y = this.robi.getPosition().y;
+            
+            if(x + this.robi.getWidth() < this.space.getWidth() && y == 0) {
+            	x += this.speed;
+            }
+            else if(x + this.robi.getWidth() == this.space.getWidth() && y + this.robi.getHeight() < this.space.getHeight()) {
+            	y += this.speed;
+            }
+            else if(y + this.robi.getHeight() == this.space.getHeight() && x > 0) {
+            	x -= this.speed;
+            }
+            else if(x == 0 && y > 0) {
+            	y -= this.speed;
+            }
+            else if(x + this.robi.getWidth() < this.space.getWidth()) {
+            	x += this.speed;
+            }
 
+            this.robi.setPosition(new Point(x, y));
+            
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+    	}
+    }
+
+    public static void main(String[] args) {
+        Exercice1_0 exercice = new Exercice1_0();
+        exercice.tourFenetre();
+    }
 }
+
