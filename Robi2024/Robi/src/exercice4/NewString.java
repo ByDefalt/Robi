84cbfbd81a942dbb8b86c026db2af16b1a3de504
Erwan <erwan.lebras27@gmail.com>
2024-04-07 16:20:43 +0200

    delete debug line

--- Robi2024/Robi/src/exercice4/NewString.java
+++ Robi2024/Robi/src/exercice4/NewString.java
@@ -7,11 +7,6 @@ public class NewString implements Command {
 
     @Override
     public Reference run(Reference receiver, SNode expr) {
-        for(int i = 0; i < expr.size(); i++) {
-        	for(int j = 0; j < expr.get(i).children().size(); j++) {
-        		System.out.println("i: " + i + ", j: " + j + ", contentss: " + expr.get(i).children().get(j).contents());
-        	}
-        }
         String text = expr.get(3).children().get(2).contents().replace("\"", "");
         
         Reference ref = null;
