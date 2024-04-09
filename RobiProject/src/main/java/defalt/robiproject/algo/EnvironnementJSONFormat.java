074625e9df08f49d4169659994548f7ed485fe95
ByDefalt <rousvalromain@gmail.com>
2024-04-09 19:08:41 +0200

    Create EnvironnementJSONFormat.java

---
+++ RobiProject/src/main/java/defalt/robiproject/algo/EnvironnementJSONFormat.java
@@ -0,0 +1,43 @@
+package defalt.robiproject.algo;
+
+import java.util.List;
+
+public class EnvironnementJSONFormat {
+
+    private String name;
+
+    private List<EnvironnementJSONFormat> children;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<EnvironnementJSONFormat> getChildren() {
+        return children;
+    }
+
+    public void setChildren(List<EnvironnementJSONFormat> children) {
+        this.children = children;
+    }
+    public EnvironnementJSONFormat(String name){
+        this.name=name;
+    }
+    public void addChildren(String nameofchildren){
+        this.children.add(new EnvironnementJSONFormat(nameofchildren));
+    }
+    public EnvironnementJSONFormat searchandadd(String name){
+        String[] split=name.split(".");
+        if(this.name==split[split.length-1]){
+            this.addChildren(split[split.length-1]);
+        }
+        for(EnvironnementJSONFormat s : children){
+            s.searchandadd(name);
+        }
+        return null;
+    }
+}
