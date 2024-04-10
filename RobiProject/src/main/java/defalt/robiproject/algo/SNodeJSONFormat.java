4a51dd91570ad7e5f53558c106e4d9950e21718e
Pierren393 <103003009+Pierren393@users.noreply.github.com>
2024-04-10 21:07:52 +0200

    Update SNodeJSONFormat.java add com

--- RobiProject/src/main/java/defalt/robiproject/algo/SNodeJSONFormat.java
+++ RobiProject/src/main/java/defalt/robiproject/algo/SNodeJSONFormat.java
@@ -9,39 +9,81 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+/**
+ * Cette classe représente un nœud S sous forme de format JSON.
+ * @author LE BRAS Erwan
+ * @author ROUSVAL Romain
+ * @author NICOLAS Pierre
+ * @author KERVRAN Maxime
+ */
 public class SNodeJSONFormat {
 
-    private String commandname;
+    private String commandname; // Nom de la commande
 
-    private List<SNodeJSONFormat> children=new ArrayList<>();
+    private List<SNodeJSONFormat> children = new ArrayList<>(); // Liste des enfants du nœud
 
-
+    /**
+     * Obtient le nom de la commande.
+     * @return Le nom de la commande.
+     */
     public String getCommandname() {
         return commandname;
     }
 
+    /**
+     * Définit le nom de la commande.
+     * @param commandname Le nom de la commande à définir.
+     */
     public void setCommandname(String commandname) {
         this.commandname = commandname;
     }
 
+    /**
+     * Obtient la liste des enfants du nœud.
+     * @return La liste des enfants du nœud.
+     */
     public List<SNodeJSONFormat> getChildren() {
         return children;
     }
 
+    /**
+     * Définit la liste des enfants du nœud.
+     * @param children La liste des enfants du nœud à définir.
+     */
     public void setChildren(List<SNodeJSONFormat> children) {
         this.children = children;
     }
-    public SNodeJSONFormat(){
+
+    /**
+     * Constructeur par défaut de la classe SNodeJSONFormat.
+     */
+    public SNodeJSONFormat() {
     }
-    public SNodeJSONFormat(String name){
-        this.commandname=name;
+
+    /**
+     * Constructeur de la classe SNodeJSONFormat avec le nom de la commande.
+     * @param name Le nom de la commande.
+     */
+    public SNodeJSONFormat(String name) {
+        this.commandname = name;
     }
-    public void addChildren(String name){
+
+    /**
+     * Ajoute un enfant au nœud.
+     * @param name Le nom de la commande de l'enfant à ajouter.
+     */
+    public void addChildren(String name) {
         this.children.add(new SNodeJSONFormat(name));
     }
+
+    /**
+     * Crée un objet SNodeJSONFormat à partir d'un objet SDefaultNode.
+     * @param defaultNode Le nœud SDefaultNode à convertir.
+     * @return Un objet SNodeJSONFormat créé à partir du nœud SDefaultNode.
+     */
     public static SNodeJSONFormat copyFromSDefaultNode(SNode defaultNode) {
         SNodeJSONFormat jsonFormatNode = new SNodeJSONFormat();
-        jsonFormatNode.setCommandname(defaultNode.contents()==null ? "null":defaultNode.contents());
+        jsonFormatNode.setCommandname(defaultNode.contents() == null ? "null" : defaultNode.contents());
         if (defaultNode.children() != null) {
             for (SNode child : defaultNode.children()) {
                 if (child instanceof SDefaultNode) {
@@ -51,13 +93,11 @@ public class SNodeJSONFormat {
         }
         return jsonFormatNode;
     }
-    @Override
-    public String toString() {
-        return "SNodeJSONFormat{" +
-                "commandname='" + commandname + '\'' +
-                ", children=" + children +
-                '}';
-    }
+
+    /**
+     * Convertit l'objet SNodeJSONFormat en format JSON.
+     * @return Le format JSON de l'objet SNodeJSONFormat.
+     */
     public String toJson() {
         Gson gson = new GsonBuilder()
                 .registerTypeAdapter(SNodeJSONFormat.class, new SNodeJSONFormatAdapter())
@@ -65,6 +105,11 @@ public class SNodeJSONFormat {
         return gson.toJson(this);
     }
 
+    /**
+     * Convertit un format JSON en objet SNodeJSONFormat.
+     * @param json Le format JSON à convertir.
+     * @return L'objet SNodeJSONFormat converti.
+     */
     public static SNodeJSONFormat fromJson(String json) {
         Gson gson = new GsonBuilder()
                 .registerTypeAdapter(SNodeJSONFormat.class, new SNodeJSONFormatAdapter())
@@ -72,29 +117,36 @@ public class SNodeJSONFormat {
         return gson.fromJson(json, SNodeJSONFormat.class);
     }
 
-    public String getCommandeChildren(){
-        int a=1;
-        StringBuilder res= new StringBuilder("( ");
-        for(SNodeJSONFormat sNodeJSONFormat : this.getChildren()){
-            res.append((Objects.equals(sNodeJSONFormat.getCommandname(), "null")) ? "\""+(a++)+"\" " : sNodeJSONFormat.getCommandname() + " ");
+    /**
+     * Obtient une représentation textuelle des commandes des enfants du nœud.
+     * @return La représentation textuelle des commandes des enfants du nœud.
+     */
+    public String getCommandeChildren() {
+        int a = 1;
+        StringBuilder res = new StringBuilder("( ");
+        for (SNodeJSONFormat sNodeJSONFormat : this.getChildren()) {
+            res.append((Objects.equals(sNodeJSONFormat.getCommandname(), "null")) ? "\"" + (a++) + "\" " : sNodeJSONFormat.getCommandname() + " ");
         }
-            res.append(")");
+        res.append(")");
         return res.toString();
     }
-    public static void main(String[] args){
-        SNode sNode=new SDefaultNode();
-        SNode sNode2=new SDefaultNode();
+
+    /**
+     * Méthode principale utilisée pour tester la classe.
+     * @param args Les arguments de la ligne de commande.
+     */
+    public static void main(String[] args) {
+        SNode sNode = new SDefaultNode();
+        SNode sNode2 = new SDefaultNode();
         sNode2.setContents("space");
-        SNode sNode3=new SDefaultNode();
+        SNode sNode3 = new SDefaultNode();
         sNode3.setContents("setColor");
-        SNode sNode4=new SDefaultNode();
+        SNode sNode4 = new SDefaultNode();
         sNode4.setContents("null");
         sNode.addChild(sNode2);
         sNode.addChild(sNode3);
         sNode.addChild(sNode4);
         SNodeJSONFormat sNodeJSONFormat = SNodeJSONFormat.copyFromSDefaultNode(sNode);
         System.out.println(sNodeJSONFormat.getCommandeChildren());
-
     }
-
 }
