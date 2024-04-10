94cdd56e485b11386ab2ce43bba7a5cd800d023e
Pierren393 <103003009+Pierren393@users.noreply.github.com>
2024-04-10 19:35:14 +0200

    Update Exercice5.java add com

--- RobiProject/src/main/java/defalt/robiproject/algo/Exercice5.java
+++ RobiProject/src/main/java/defalt/robiproject/algo/Exercice5.java
@@ -1,5 +1,3 @@
-package defalt.robiproject.algo;
-
 import defalt.robiproject.graphicLayer.*;
 import defalt.robiproject.parser.SNode;
 import defalt.robiproject.parser.SParser;
@@ -9,99 +7,70 @@ import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
 
-/*
+/**
+ * La classe Exercice5 contient la méthode main pour exécuter les scripts
+ * prédéfinis. Elle démontre l'utilisation des commandes définies et leur
+ * exécution dans un espace graphique.
  * 
-(space add robi (rect.class new))
-(robi translate 130 50)
-(robi setColor yellow)
-(space add momo (oval.class new))
-(momo setColor red)
-(momo translate 80 80)
-(space add pif (image.class new alien.gif))
-(pif translate 100 0)
-(space add hello (label.class new "Hello world"))
-(hello translate 10 10)
-(hello setColor black)
-
-
-(space setDim 150 120)
-(space add robi (rect.class new))
-(space.robi setColor white)
-(space.robi setDim 100 100)
-(space.robi translate 20 10)
-(space.robi add im (image.class new alien.gif))
-(space.robi.im translate 20 20))
-*/
+ * @author LE BRAS Erwan
+ * @author ROUSVAL Romain
+ * @author NICOLAS Pierre
+ * @author KERVRAN Maxime
+ */
 public class Exercice5 {
-	Environment environment = new Environment();
-	String script = "(space setDim 150 120)\n" +
-            "(space add robi (rect.class new))\n" +
-            "(space.robi setColor white)\n" +
-            "(space.robi setDim 100 100)\n" +
-            "(space.robi translate 20 10)\n" +
-            "(space.robi add im (image.class new alien.gif))\n" +
-            "(space.robi.im translate 20 20)";
-	
-	String script2 = "(space add robi (Rect new))\n"
-			+ "(space.robi setDim 50 50)\n"
-			+ "(space.robi add robi (Rect new))\n"
-			+ "(space.robi.robi setColor red)\n"
-			+ "(space.robi setColor white)";
-	
-	String script2Del = "(space add robi (rect.class new))\n"
-			+ "(space.robi setDim 50 50)\n"
-			+ "(space.robi add robi (rect.class new))\n"
-			+ "(space.robi.robi setColor red)\n"
-			+ "(space.robi setColor white)"
-			+ "(space del robi)";
+	private final Environment environment = new Environment();
+	private final String script = "(space setDim 150 120)\n" + "(space add robi (rect.class new))\n"
+			+ "(space.robi setColor white)\n" + "(space.robi setDim 100 100)\n" + "(space.robi translate 20 10)\n"
+			+ "(space.robi add im (image.class new alien.gif))\n" + "(space.robi.im translate 20 20)";
 
-
-    public Exercice5() {
-    	GSpace space = new GSpace("Exercice 5", new Dimension(800, 500));
+	/**
+	 * Construit un objet Exercice5 et initialise l'espace graphique et
+	 * l'environnement avec des commandes.
+	 */
+	public Exercice5() {
+		GSpace space = new GSpace("Exercice 5", new Dimension(800, 500));
 		space.open();
 
 		Reference spaceRef = new Reference(space);
 		Reference rectClassRef = new Reference(GRect.class);
-		Reference ovalClassRef = new Reference(GOval.class);
 		Reference imageClassRef = new Reference(GImage.class);
-		Reference stringClassRef = new Reference(GString.class);
 
 		spaceRef.addCommand("setColor", new SetColor());
 		spaceRef.addCommand("sleep", new SleepCommand());
-
 		spaceRef.addCommand("add", new AddElement(environment));
 		spaceRef.addCommand("del", new DelElement(environment));
-		
 		spaceRef.addCommand("setDim", new SetDim());
 		spaceRef.addCommand("translate", new Translate());
-		
+
 		rectClassRef.addCommand("new", new NewElement(environment));
-		ovalClassRef.addCommand("new", new NewElement(environment));
 		imageClassRef.addCommand("new", new NewImage());
-		stringClassRef.addCommand("new", new NewString());
 
 		environment.addReference("space", spaceRef);
 		environment.addReference("rect.class", rectClassRef);
-		environment.addReference("oval.class", ovalClassRef);
 		environment.addReference("image.class", imageClassRef);
-		environment.addReference("label.class", stringClassRef);
-    }
+	}
 
-    public void oneShot(String script) {
-        SParser<SNode> parser = new SParser<>();
-        List<SNode> compiled;
-        try {
-            compiled = parser.parse(script2);
-            Iterator<SNode> itor = compiled.iterator();
-            while (itor.hasNext()) {
-                new Interpreter().compute(environment, itor.next());
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    public static void main(String[] args) {
-        Exercice5 exo = new Exercice5();
-        exo.oneShot(exo.script);
-    }
+	/**
+	 * Exécute le script fourni en le parsant et en itérant sur les nœuds
+	 * résultants.
+	 *
+	 * @param script Le script à exécuter.
+	 */
+	public void oneShot(String script) {
+		SParser<SNode> parser = new SParser<>();
+		try {
+			List<SNode> compiled = parser.parse(script);
+			Iterator<SNode> iterator = compiled.iterator();
+			while (iterator.hasNext()) {
+				new Interpreter().compute(environment, iterator.next());
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		Exercice5 exo = new Exercice5();
+		exo.oneShot(exo.script);
+	}
 }
