75330fc5867fedee4331c451a19fd603d7fab025
Pierren393 <103003009+Pierren393@users.noreply.github.com>
2024-04-10 19:30:13 +0200

    Update MainInterfaceUtilisateur.java add com

--- RobiProject/src/main/java/defalt/robiproject/ui/MainInterfaceUtilisateur.java
+++ RobiProject/src/main/java/defalt/robiproject/ui/MainInterfaceUtilisateur.java
@@ -6,28 +6,47 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * Classe principale pour l'interface utilisateur.
+ * 
+ * @author LE BRAS Erwan
+ * @author ROUSVAL Romain
+ * @author NICOLAS Pierre
+ * @author KERVRAN Maxime
+ */
 public class MainInterfaceUtilisateur extends Application {
+	/**
+	 * Méthode de démarrage de l'application JavaFX.
+	 * 
+	 * @param primaryStage Le stage principal de l'application.
+	 * @throws Exception Si une exception se produit.
+	 */
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("interface.fxml"));
+		Parent root = loader.load();
+		primaryStage.setTitle("FXML Example");
+		primaryStage.setScene(new Scene(root, 900, 425));
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("interface.fxml"));
-        Parent root = loader.load();
-        primaryStage.setTitle("FXML Example");
-        primaryStage.setScene(new Scene(root, 900, 425));
+		// Récupérer une référence au contrôleur après le chargement du fichier FXML
+		InterfaceControleur controller = loader.getController();
 
-        // Récupérer une référence au contrôleur après le chargement du fichier FXML
-        InterfaceControleur controller = loader.getController();
+		// Ajout d'un gestionnaire pour l'événement de fermeture de la fenêtre
+		// principale
+		primaryStage.setOnCloseRequest(event -> {
+			// Appeler la méthode du contrôleur pour arrêter le thread et la connexion
+			controller.stopThreadAndConnection();
+		});
 
-        // Ajout d'un gestionnaire pour l'événement de fermeture de la fenêtre principale
-        primaryStage.setOnCloseRequest(event -> {
-            // Appeler la méthode du contrôleur pour arrêter le thread et la connexion
-            controller.stopThreadAndConnection();
-        });
+		primaryStage.show();
+	}
 
-        primaryStage.show();
-    }
-
-    public static void main(String[] args) {
-        launch(args);
-    }
+	/**
+	 * Méthode principale pour lancer l'application.
+	 * 
+	 * @param args Les arguments de la ligne de commande.
+	 */
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
