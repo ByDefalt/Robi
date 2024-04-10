df806d1f8e217e6e6252846d99addb2e5a7eb5bd
Pierren393 <103003009+Pierren393@users.noreply.github.com>
2024-04-10 21:09:27 +0200

    Update SNodeJSONFormatAdapter.java add com

--- RobiProject/src/main/java/defalt/robiproject/algo/SNodeJSONFormatAdapter.java
+++ RobiProject/src/main/java/defalt/robiproject/algo/SNodeJSONFormatAdapter.java
@@ -4,7 +4,23 @@ import com.google.gson.*;
 
 import java.lang.reflect.Type;
 
+/**
+ * Cette classe est un adaptateur pour la sérialisation et la désérialisation d'objets SNodeJSONFormat en JSON.
+ *
+ * @author LE BRAS Erwan
+ * @author ROUSVAL Romain
+ * @author NICOLAS Pierre
+ * @author KERVRAN Maxime
+ */
 public class SNodeJSONFormatAdapter implements JsonSerializer<SNodeJSONFormat>, JsonDeserializer<SNodeJSONFormat> {
+
+    /**
+     * Convertit un objet SNodeJSONFormat en élément JSON.
+     * @param src L'objet SNodeJSONFormat à convertir.
+     * @param typeOfSrc Le type de l'objet source.
+     * @param context Le contexte de sérialisation.
+     * @return L'élément JSON correspondant à l'objet SNodeJSONFormat.
+     */
     @Override
     public JsonElement serialize(SNodeJSONFormat src, Type typeOfSrc, JsonSerializationContext context) {
         JsonObject jsonObject = new JsonObject();
@@ -17,6 +33,14 @@ public class SNodeJSONFormatAdapter implements JsonSerializer<SNodeJSONFormat>, 
         return jsonObject;
     }
 
+    /**
+     * Convertit un élément JSON en objet SNodeJSONFormat.
+     * @param json L'élément JSON à convertir.
+     * @param typeOfT Le type de l'objet cible.
+     * @param context Le contexte de désérialisation.
+     * @return L'objet SNodeJSONFormat correspondant à l'élément JSON.
+     * @throws JsonParseException Si une erreur de désérialisation se produit.
+     */
     @Override
     public SNodeJSONFormat deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
         JsonObject jsonObject = json.getAsJsonObject();
