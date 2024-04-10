855d290d112a89f977619d43b1a6b75a8fe945b2
Pierren393 <103003009+Pierren393@users.noreply.github.com>
2024-04-10 19:36:00 +0200

    Update NewElement.java add com

--- RobiProject/src/main/java/defalt/robiproject/algo/NewElement.java
+++ RobiProject/src/main/java/defalt/robiproject/algo/NewElement.java
@@ -2,40 +2,60 @@ package defalt.robiproject.algo;
 
 import java.lang.reflect.InvocationTargetException;
 
-
 import defalt.robiproject.graphicLayer.GElement;
 import defalt.robiproject.parser.SNode;
 
+/**
+ * Cette classe représente une commande pour créer un nouvel élément graphique.
+ * 
+ * @author LE BRAS Erwan
+ * @author ROUSVAL Romain
+ * @author NICOLAS Pierre
+ * @author KERVRAN Maxime
+ */
 public class NewElement implements Command {
 	private Environment environment;
-	
+
+	/**
+	 * Constructeur de la classe NewElement.
+	 * 
+	 * @param environment L'environnement dans lequel l'élément est créé.
+	 */
 	public NewElement(Environment environment) {
 		this.environment = environment;
 	}
-	
+
+	/**
+	 * Exécute la commande pour créer un nouvel élément graphique.
+	 * 
+	 * @param reference La référence de l'élément à créer.
+	 * @param method    Le nœud de l'arbre syntaxique représentant la méthode de
+	 *                  création de l'élément.
+	 * @return La référence de l'élément créé.
+	 */
 	public Reference run(Reference reference, SNode method) {
 		try {
 			@SuppressWarnings("unchecked")
-			GElement e = ((Class<GElement>) reference.getReceiver()).getDeclaredConstructor().newInstance();
-			Reference ref = new Reference(e);
+			GElement element = ((Class<GElement>) reference.getReceiver()).getDeclaredConstructor().newInstance();
+			Reference ref = new Reference(element);
 			ref.addCommand("setColor", new SetColor());
 			ref.addCommand("translate", new Translate());
 			ref.addCommand("setDim", new SetDim());
 			ref.addCommand("add", new AddElement(this.environment));
 			return ref;
 		} catch (InstantiationException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
-        } catch (SecurityException e) {
-            e.printStackTrace();
-        }
-        return null;
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			e.printStackTrace();
+		} catch (NoSuchMethodException e) {
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 }
