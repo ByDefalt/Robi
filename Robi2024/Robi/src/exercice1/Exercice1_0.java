5ee12d6efb1a956bfdc8189bc2cdc2e53b1c20b8
Pierren393 <103003009+Pierren393@users.noreply.github.com>
2024-04-10 18:13:18 +0200

    Update Exercice1_0.java com

--- Robi2024/Robi/src/exercice1/Exercice1_0.java
+++ Robi2024/Robi/src/exercice1/Exercice1_0.java
@@ -7,53 +7,71 @@ import java.awt.Point;
 import graphicLayer.GRect;
 import graphicLayer.GSpace;
 
+/**
+ * Classe principale pour l'exercice 1. Cette classe crée un espace graphique et
+ * un rectangle qui tourne autour du périmètre de la fenêtre.
+ * 
+ * @author LE BRAS Erwan
+ * @author ROUSVAL Romain
+ * @author NICOLAS Pierre
+ * @author KERVRAN Maxime
+ */
 public class Exercice1_0 {
-    GSpace space;
-    GRect robi;
-    int speed = 1;
+  GSpace space;
+  GRect robi;
+  int speed = 1;
 
-    public Exercice1_0() {
-        this.space = new GSpace("Exercice 1", new Dimension(400, 400));
-        this.robi = new GRect();
-        this.space.addElement(robi);
-        this.space.open();
-        this.robi.setColor(new Color((int) (Math.random() * 0x1000000)));
+  /**
+   * Constructeur de la classe Exercice1_0. Initialise l'espace graphique et le
+   * rectangle.
+   */
+  public Exercice1_0() {
+    this.space = new GSpace("Exercice 1", new Dimension(400, 400));
+    this.robi = new GRect();
+    this.space.addElement(robi);
+    this.space.open();
+    this.robi.setColor(new Color((int) (Math.random() * 0x1000000)));
+  }
+
+  /**
+   * Méthode pour faire tourner le rectangle autour de la fenêtre.
+   */
+  public void tourFenetre() {
+    while (true) {
+      int x = this.robi.getPosition().x;
+      int y = this.robi.getPosition().y;
+
+      if (x + this.robi.getWidth() < this.space.getWidth() && y == 0) {
+        x += this.speed;
+      } else if (x + this.robi.getWidth() == this.space.getWidth()
+          && y + this.robi.getHeight() < this.space.getHeight()) {
+        y += this.speed;
+      } else if (y + this.robi.getHeight() == this.space.getHeight() && x > 0) {
+        x -= this.speed;
+      } else if (x == 0 && y > 0) {
+        y -= this.speed;
+      } else if (x + this.robi.getWidth() < this.space.getWidth()) {
+        x += this.speed;
+      }
+
+      this.robi.setPosition(new Point(x, y));
+
+      try {
+        Thread.sleep(10);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
     }
+  }
 
-    public void tourFenetre() {
-    	while(true) {
-    		int x = this.robi.getPosition().x;
-            int y = this.robi.getPosition().y;
-            
-            if(x + this.robi.getWidth() < this.space.getWidth() && y == 0) {
-            	x += this.speed;
-            }
-            else if(x + this.robi.getWidth() == this.space.getWidth() && y + this.robi.getHeight() < this.space.getHeight()) {
-            	y += this.speed;
-            }
-            else if(y + this.robi.getHeight() == this.space.getHeight() && x > 0) {
-            	x -= this.speed;
-            }
-            else if(x == 0 && y > 0) {
-            	y -= this.speed;
-            }
-            else if(x + this.robi.getWidth() < this.space.getWidth()) {
-            	x += this.speed;
-            }
-
-            this.robi.setPosition(new Point(x, y));
-            
-            try {
-                Thread.sleep(10);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-    	}
-    }
-
-    public static void main(String[] args) {
-        Exercice1_0 exercice = new Exercice1_0();
-        exercice.tourFenetre();
-    }
+  /**
+   * Méthode principale, point d'entrée de l'application. Crée une instance de la
+   * classe Exercice1_0 et lance la méthode pour faire tourner le rectangle.
+   * 
+   * @param args Arguments de la ligne de commande (non utilisés).
+   */
+  public static void main(String[] args) {
+    Exercice1_0 exercice = new Exercice1_0();
+    exercice.tourFenetre();
+  }
 }
-
