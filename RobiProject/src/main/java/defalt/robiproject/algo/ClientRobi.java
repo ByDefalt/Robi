5412c744d7c837516632c5b7fce6178017aed799
Pierren393 <103003009+Pierren393@users.noreply.github.com>
2024-04-10 19:33:08 +0200

    Update ClientRobi.java

--- RobiProject/src/main/java/defalt/robiproject/algo/ClientRobi.java
+++ RobiProject/src/main/java/defalt/robiproject/algo/ClientRobi.java
@@ -1,7 +1,15 @@
 package defalt.robiproject.algo;
 
-import  defalt.robiproject.socket.Client;
-public class ClientRobi extends Client{
+import defalt.robiproject.socket.Client;
 
+/**
+ * Cette classe représente un client Robi qui étend la classe Client.
+ * 
+ * @author LE BRAS Erwan
+ * @author ROUSVAL Romain
+ * @author NICOLAS Pierre
+ * @author KERVRAN Maxime
+ */
+public class ClientRobi extends Client {
 
 }
