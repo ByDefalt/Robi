c22e05fb3d62326103a9faed536f91617722272c
Pierren393 <103003009+Pierren393@users.noreply.github.com>
2024-04-10 20:23:37 +0200

    Update EnvironnementJSONFormatAdapter.java add com

--- RobiProject/src/main/java/defalt/robiproject/algo/EnvironnementJSONFormatAdapter.java
+++ RobiProject/src/main/java/defalt/robiproject/algo/EnvironnementJSONFormatAdapter.java
@@ -4,7 +4,23 @@ import com.google.gson.*;
 
 import java.lang.reflect.Type;
 
-public class EnvironnementJSONFormatAdapter implements JsonSerializer<EnvironnementJSONFormat>,JsonDeserializer<EnvironnementJSONFormat> {
+/**
+ * Cet adaptateur GSON est utilisé pour sérialiser et désérialiser des objets EnvironnementJSONFormat.
+ * Il est utilisé pour convertir des objets EnvironnementJSONFormat en format JSON et vice versa.
+ * @author LE BRAS Erwan
+ * @author ROUSVAL Romain
+ * @author NICOLAS Pierre
+ * @author KERVRAN Maxime
+ */
+public class EnvironnementJSONFormatAdapter implements JsonSerializer<EnvironnementJSONFormat>, JsonDeserializer<EnvironnementJSONFormat> {
+
+    /**
+     * Méthode pour sérialiser un objet EnvironnementJSONFormat en format JSON.
+     * @param src L'objet EnvironnementJSONFormat à sérialiser.
+     * @param typeOfSrc Le type de l'objet source.
+     * @param context Le contexte de la sérialisation JSON.
+     * @return Un élément JSON représentant l'objet EnvironnementJSONFormat.
+     */
     @Override
     public JsonElement serialize(EnvironnementJSONFormat src, Type typeOfSrc, JsonSerializationContext context) {
         JsonObject jsonObject = new JsonObject();
@@ -17,6 +33,14 @@ public class EnvironnementJSONFormatAdapter implements JsonSerializer<Environnem
         return jsonObject;
     }
 
+    /**
+     * Méthode pour désérialiser un objet EnvironnementJSONFormat à partir du format JSON.
+     * @param json Le format JSON à désérialiser.
+     * @param typeOfT Le type de l'objet à désérialiser.
+     * @param context Le contexte de la désérialisation JSON.
+     * @return L'objet EnvironnementJSONFormat désérialisé.
+     * @throws JsonParseException Si une erreur se produit lors de la désérialisation JSON.
+     */
     @Override
     public EnvironnementJSONFormat deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
         JsonObject jsonObject = json.getAsJsonObject();
