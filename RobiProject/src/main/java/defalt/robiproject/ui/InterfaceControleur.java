aba7e6494378d7689cb97c7833cb2ba1783deaed
ByDefalt <rousvalromain@gmail.com>
2024-04-11 19:38:30 +0200

    add com

--- RobiProject/src/main/java/defalt/robiproject/ui/InterfaceControleur.java
+++ RobiProject/src/main/java/defalt/robiproject/ui/InterfaceControleur.java
@@ -66,8 +66,15 @@ public class InterfaceControleur extends Client {
     private int possition=0;
 
     /**
-	 * Action du bouton de connexion.
-	 */
+     * Méthode exécutée lorsqu'un utilisateur clique sur le bouton de connexion.
+     * Cette méthode vérifie si les champs d'entrée pour l'adresse IP et le port ne sont pas vides.
+     * Si les champs ne sont pas vides, elle tente d'établir une connexion en utilisant les informations fournies.
+     * Si la connexion réussit, elle lance un thread pour recevoir les messages entrants.
+     * Elle met à jour l'état de la connexion et affiche "Connecté" dans le label correspondant.
+     * En cas d'erreur lors de la connexion, elle affiche "erreur de connexion" dans le label.
+     * Si le port spécifié n'est pas un entier valide, elle affiche "port non valide".
+     * Si l'un des champs d'entrée est vide, elle affiche "entrée non valide".
+     */
     @FXML
     private void actionBoutonConnexion() {
         if(entreeIp.getText()!=null && !entreeIp.getText().isEmpty() && entreePort.getText()!=null && !entreePort.getText().isEmpty()){
@@ -87,10 +94,13 @@ public class InterfaceControleur extends Client {
             showError("entree non valide");
         }
     }
-    
+
     /**
-	 * Action du bouton de déconnexion.
-	 */
+     * Déclenche l'action de déconnexion lorsque le bouton de déconnexion est pressé.
+     * Cette méthode vérifie d'abord si une connexion est établie avant de procéder à la déconnexion.
+     * Si une connexion est active, elle la ferme en appelant la méthode stopSocket() héritée de la classe parente.
+     * Si une IOException survient lors de la fermeture de la connexion, une erreur est affichée.
+     */
     @FXML
     private void actionBoutonDeconnexion() {
         if(IsConnected){
@@ -104,8 +114,12 @@ public class InterfaceControleur extends Client {
     }
 
     /**
-	 * Action du bouton d'envoi de commande.
-	 */
+     * Méthode pour gérer l'action du bouton "Envoyer" lorsqu'il est activé.
+     * Si une connexion est établie, le texte entré dans le champ d'entrée est ajouté à la zone de commande,
+     * puis envoyé au serveur sous forme de commande JSON via une socket.
+     * Une fois la commande envoyée, le champ d'entrée est effacé.
+     * En cas d'erreur lors de l'envoi, une erreur est affichée.
+     */
     @FXML
     private void actionBoutonEnvoyer() {
         if(IsConnected){
@@ -119,10 +133,21 @@ public class InterfaceControleur extends Client {
             }
         }
     }
-    
-	/**
-	 * Action du bouton d'ouverture de fichier.
-	 */
+
+    /**
+     * Méthode pour gérer l'action de clic sur le bouton d'ouverture de fichier.
+     * Ouvre une boîte de dialogue permettant à l'utilisateur de sélectionner un fichier à ouvrir.
+     * Si un fichier est sélectionné, son contenu est lu ligne par ligne et affiché dans un composant de texte.
+     *
+     * @implNote Cette méthode utilise JavaFX pour afficher une boîte de dialogue de sélection de fichier.
+     * @implNote Le contenu du fichier sélectionné est lu à l'aide d'un BufferedReader pour une efficacité accrue.
+     * @implNote Les erreurs liées à la lecture du fichier sont gérées et affichent un message d'erreur approprié.
+     *
+     * @see FileChooser
+     * @see BufferedReader
+     * @see FileReader
+     * @see IOException
+     */
     @FXML
     private void actionBoutonOpen() {
         FileChooser fileChooser = new FileChooser();
@@ -143,8 +168,11 @@ public class InterfaceControleur extends Client {
     }
 
     /**
-	 * Action du bouton d'exécution de commande.
-	 */
+     * Cette méthode est appelée lorsque le bouton d'exécution est activé. Elle vérifie d'abord si une connexion est établie.
+     * Si c'est le cas, elle crée une commande socket appropriée en fonction de l'état de la case à cocher "Pas" et envoie cette commande.
+     * Si la case à cocher "Pas" est sélectionnée, la commande "executer_pas" est créée, sinon la commande "executer_block" est utilisée.
+     * La position est également mise à jour en fonction de l'état de la case à cocher "Pas".
+     */
     @FXML
     private void actionBoutonExecution() {
         if(IsConnected){
@@ -159,8 +187,10 @@ public class InterfaceControleur extends Client {
     }
 
     /**
-	 * Action du bouton de commande précédente.
-	 */
+     * Cette méthode est appelée lorsque le bouton "Précédent" est déclenché.
+     * Elle envoie une commande "precedent" via un socket si l'utilisateur est connecté.
+     * Si la position est supérieure à 1, elle efface tous les enfants de l'élément racine.
+     */
     @FXML
     private void actionBoutonPrecedent() {
         if(IsConnected){
@@ -173,10 +203,11 @@ public class InterfaceControleur extends Client {
             }
         }
     }
-    
-	/**
-	 * Action du bouton de commande suivante.
-	 */
+
+    /**
+     * Méthode invoquée lorsqu'un utilisateur clique sur le bouton "Suivant".
+     * Elle envoie une commande "suivant" via une socket si l'application est connectée.
+     */
     @FXML
     private void actionBoutonSuivant() {
         if(IsConnected){
@@ -188,26 +219,37 @@ public class InterfaceControleur extends Client {
             }
         }
     }
-    
-	/**
-	 * Action du bouton de sortie.
-	 */
+
+    /**
+     * Méthode appelée lorsqu'un utilisateur clique sur le bouton de fermeture.
+     * Si une connexion est établie, cette méthode interrompt le thread de communication,
+     * ferme la connexion, et quitte l'application.
+     */
     @FXML
     private void actionBoutonQuit() {
         if(IsConnected) {
             try {myThread.interrupt();
                 IsConnected = false;
                 super.stopSocket();
-                Platform.exit();
             } catch (IOException e) {
                 showError("Erreur lors de la fermeture des services");
             }
         }
+        Platform.exit();
     }
 
     /**
-	 * Méthode pour recevoir les messages du serveur.
-	 */
+     * Méthode pour recevoir et traiter les messages provenant d'un socket.
+     * Cette méthode est exécutée dans une boucle tant que le socket n'est pas fermé.
+     * Les messages reçus sont lus sous forme d'objets et traités en fonction de leur type.
+     * Les différentes actions à effectuer dépendent du contenu des messages reçus.
+     * Les exceptions sont capturées et gérées de manière à afficher des messages appropriés à l'utilisateur.
+     *
+     * @throws EOFException Si le serveur ferme la connexion.
+     * @throws SocketException Si une erreur se produit sur la connexion, par exemple si le client se déconnecte ou si le serveur est arrêté.
+     * @throws IOException Si une erreur d'entrée/sortie survient pour d'autres raisons.
+     * @throws ClassNotFoundException Si la classe de l'objet reçu n'a pas été trouvée.
+     */
     @Override
     public final void receiveMessage() {
         while (!getSocket().isClosed()) {
@@ -256,7 +298,13 @@ public class InterfaceControleur extends Client {
         }
     }
 
-
+    /**
+     * Convertit une chaîne encodée en Base64 représentant une image en une Image JavaFX et l'affiche.
+     *
+     * @param base64 Une chaîne encodée en Base64 représentant l'image.
+     * @throws IllegalArgumentException Si la chaîne Base64 fournie est vide ou nulle.
+     * @throws IllegalStateException    Si une erreur survient lors du processus de conversion ou si l'image ne peut pas être affichée.
+     */
     private void Base64ToImage(String base64){
         byte[] imageBytes = Base64.getDecoder().decode(base64);
         if (imageBytes.length == 0) {
@@ -295,6 +343,11 @@ public class InterfaceControleur extends Client {
             alert.showAndWait();
         });
     }
+    /**
+     * Construit un arbre d'environnement à partir d'une liste d'objets au format JSON représentant les environnements.
+     *
+     * @param environnement La liste d'objets au format JSON représentant les environnements.
+     */
     public void TreeConstructEnvironnement(ArrayList<EnvironnementJSONFormat> environnement){
         if(environnement!=null) {
             TreeItem<String> rootItem = new TreeItem<>("Root");
@@ -310,6 +363,12 @@ public class InterfaceControleur extends Client {
         }
     }
 
+    /**
+     * Ajoute les enfants de l'environnement spécifié à l'arborescence représentée par l'élément d'arbre donné.
+     *
+     * @param environement L'environnement au format JSON contenant les enfants à ajouter.
+     * @param Item L'élément d'arbre auquel les enfants seront ajoutés.
+     */
     public final void TreeAddChildrenEnvironnement(EnvironnementJSONFormat environement, TreeItem<String> Item){
         for(EnvironnementJSONFormat env :environement.getChildren()){
             TreeItem<String> newItem = new TreeItem<>(env.getName());
@@ -322,6 +381,11 @@ public class InterfaceControleur extends Client {
 
     TreeItem<String> rootItem = new TreeItem<>("Root");
 
+    /**
+     * Construit un arbre de nœuds à partir des données fournies au format JSON pour les nœuds S.
+     *
+     * @param sNodeJSONFormat Les données au format JSON représentant le nœud S à utiliser pour construire l'arbre.
+     */
     public void TreeConstructSNodes(SNodeJSONFormat sNodeJSONFormat){
         if(sNodeJSONFormat!=null) {
             TreeItem<String> newItem = new TreeItem<>(sNodeJSONFormat.getCommandeChildren());
@@ -334,7 +398,12 @@ public class InterfaceControleur extends Client {
             });
         }
     }
-
+    /**
+     * Ajoute récursivement les enfants du nœud S spécifié à l'élément d'arbre donné.
+     *
+     * @param sNodeJSONFormat Les données au format JSON représentant le nœud S dont les enfants doivent être ajoutés.
+     * @param Item L'élément d'arbre auquel les enfants doivent être ajoutés.
+     */
     public final void TreeAddChildrenSNodes(SNodeJSONFormat sNodeJSONFormat, TreeItem<String> Item){
         if(!sNodeJSONFormat.getChildren().isEmpty()) {
             int a = 1;
@@ -349,8 +418,11 @@ public class InterfaceControleur extends Client {
     }
 
     /**
-	 * Arrête le thread et la connexion.
-	 */
+     * Arrête le thread et ferme la connexion.
+     * Cette méthode arrête le thread associé à la connexion en cours et ferme la connexion réseau.
+     *
+     * @throws IOException si une erreur survient lors de la fermeture de la connexion
+     */
     public final void stopThreadAndConnection() {
             try {
                 super.stopSocket();
