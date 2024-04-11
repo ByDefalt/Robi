5213d8d8fef2981a95a713fd13685a992529def4
Erwan <erwan.lebras27@gmail.com>
2024-04-12 01:18:50 +0200

    fix exercice 4.1

--- Robi2024/Robi/src/exercice4/AddElement.java
+++ Robi2024/Robi/src/exercice4/AddElement.java
@@ -35,8 +35,7 @@ public class AddElement implements Command {
 		GSpace space = (GSpace) receiver.getReceiver();
 		String elementType = method.get(3).get(0).contents();
 
-		Reference refElementClass = this.environment.getReferenceByName(elementType);
-
+		Reference refElementClass = this.getRefererenceClass(elementType);
 		try {
 			Command newElementCommand = getNewElementCommand(refElementClass, elementType);
 			if (newElementCommand != null) {
@@ -54,28 +53,39 @@ public class AddElement implements Command {
 	}
 
 	/**
-	 * Obtient la commande pour créer un nouvel élément graphique.
+	 * Méthode pour obtenir la référence de la classe d'élément.
 	 * 
-	 * @param refElementClass La référence de la classe de l'élément.
-	 * @param elementType     Le type de l'élément.
-	 * @return La commande pour créer un nouvel élément graphique.
+	 * @param elementType Le type de l'élément.
+	 * @return La référence de la classe d'élément.
 	 */
-	private Command getNewElementCommand(Reference refElementClass, String elementType) {
-		switch (elementType) {
-		case "GRect":
-			return (NewElement) this.environment.getReferenceByName("rect.class").getCommandByName("new");
-		case "GOval":
-			return (NewElement) this.environment.getReferenceByName("oval.class").getCommandByName("new");
-		case "rect.class":
-			return (NewElement) refElementClass.getCommandByName("new");
-		case "oval.class":
-			return (NewElement) refElementClass.getCommandByName("new");
-		case "image.class":
-			return (NewImage) refElementClass.getCommandByName("new");
-		case "label.class":
-			return (NewString) refElementClass.getCommandByName("new");
-		default:
-			return null;
-		}
-	}
+	private Reference getRefererenceClass(String elementType) {
+        if(elementType.equals("Rect")) {
+            return this.environment.getReferenceByName("rect.class");
+        } else if(elementType.equals("Oval")) {
+            return this.environment.getReferenceByName("oval.class");
+        } else if(elementType.equals("Image")) {
+            return this.environment.getReferenceByName("image.class");
+        } else if(elementType.equals("Label")) {
+            return this.environment.getReferenceByName("label.class");
+        }
+        return this.environment.getReferenceByName(elementType);
+    }
+
+	/**
+	 * Méthode pour obtenir la commande de création d'un nouvel élément.
+	 * 
+	 * @param refElementClass La référence de la classe d'élément.
+	 * @param elementType     Le type de l'élément.
+	 * @return La commande de création d'un nouvel élément.
+	 */
+    private Command getNewElementCommand(Reference refElementClass, String elementType) {
+        if(elementType.equals("Rect") || elementType.equals("rect.class") || elementType.equals("Oval") || elementType.equals("oval.class")) {
+            return (NewElement) refElementClass.getCommandByName("new");
+        } else if(elementType.equals("Label") || elementType.equals("label.class")) {
+            return (NewString) refElementClass.getCommandByName("new");
+        } else if(elementType.equals("Image") || elementType.equals("image.class")) {
+            return (NewImage) refElementClass.getCommandByName("new");
+        }
+        return null;
+    }
 }
