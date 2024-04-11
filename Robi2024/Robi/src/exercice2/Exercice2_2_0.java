26bf1deab4a54e4c0cb44f524353bee6f3c4708b
maxime <maxime.kervran.92@gmail.com>
2024-04-11 14:59:09 +0200

    suite merge

--- Robi2024/Robi/src/exercice2/Exercice2_2_0.java
+++ Robi2024/Robi/src/exercice2/Exercice2_2_0.java
@@ -12,17 +12,48 @@ import graphicLayer.GSpace;
 import stree.parser.SNode;
 import stree.parser.SParser;
 
+/**
+ * Classe principale pour l'exercice 2_2_0. Cette classe crée un espace graphique
+ * et un rectangle, puis exécute un script pour effectuer différentes actions telles que
+ * changer la couleur de l'espace ou du rectangle, et déplacer le rectangle.
+ * 
+ * @author LE BRAS Erwan
+ * @author ROUSVAL Romain
+ * @author NICOLAS Pierre
+ * @author KERVRAN Maxime
+ */
 public class Exercice2_2_0 {
+<<<<<<< HEAD
     public GSpace space = new GSpace("Exercice 2_1", new Dimension(200, 100));
     public GRect robi = new GRect();
     String script = "(space setColor white) " + "(robi setColor red) " + "(robi translate 10 0) " + "(space sleep 100) " + "(robi translate 0 10) " + "(space sleep 100) " + "(robi translate -10 0) " + "(space sleep 100) " + "(robi translate 0 -10)";
+=======
+    GSpace space = new GSpace("Exercice 2_1", new Dimension(200, 100));
+    GRect robi = new GRect();
+    String script = "(space setColor white) " +
+                    "(robi setColor red) " +
+                    "(robi translate 10 0) " +
+                    "(space sleep 100) " +
+                    "(robi translate 0 10) " +
+                    "(space sleep 100) " +
+                    "(robi translate -10 0) " +
+                    "(space sleep 100) " +
+                    "(robi translate 0 -10)";
+>>>>>>> 1a688b8e99892dffe5cd195b5dbd9c3d807d036c
 
+    /**
+     * Constructeur de la classe Exercice2_2_0. Initialise l'espace graphique, le
+     * rectangle et exécute le script.
+     */
     public Exercice2_2_0() {
-        this.space.addElement(this.robi);
-        this.space.open();
-        this.runScript();
+        this.space.addElement(this.robi); 
+        this.space.open(); 
+        this.runScript(); 
     }
 
+    /**
+     * Méthode pour exécuter le script.
+     */
     private void runScript() {
         SParser<SNode> parser = new SParser<>();
         List<SNode> rootNodes = null;
@@ -33,19 +64,25 @@ public class Exercice2_2_0 {
         }
         Iterator<SNode> itor = rootNodes.iterator();
         while (itor.hasNext()) {
-            this.run(itor.next());
+            this.run(itor.next()); 
         }
     }
 
+    /**
+     * Méthode pour exécuter un nœud du script.
+     * 
+     * @param expr Le nœud à exécuter.
+     */
     private void run(SNode expr) {
         if (!expr.isLeaf()) {
-            List<SNode> children = expr.children();
-            String cible = children.get(0).contents();
-            String commande = children.get(1).contents();
+            List<SNode> children = expr.children(); 
+            String cible = children.get(0).contents(); 
+            String commande = children.get(1).contents(); 
+            
             
             switch (cible) {
                 case "space":
-                    handleSpaceCommand(commande, children);
+                    handleSpaceCommand(commande, children); 
                     break;
                 case "robi":
                     handleRobiCommand(commande, children);
@@ -54,14 +91,20 @@ public class Exercice2_2_0 {
         }
     }
 
+    /**
+     * Méthode pour gérer les commandes de l'espace.
+     * 
+     * @param commande La commande à exécuter.
+     * @param params   Les paramètres de la commande.
+     */
     private void handleSpaceCommand(String commande, List<SNode> params) {
         switch (commande) {
             case "setColor":
-            	this.space.setColor(getColorFromString(params.get(2).contents()));
+                this.space.setColor(getColorFromString(params.get(2).contents()));
                 break;
             case "sleep":
                 try {
-                    Thread.sleep(Integer.parseInt(params.get(2).contents()));
+                    Thread.sleep(Integer.parseInt(params.get(2).contents())); 
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -69,10 +112,16 @@ public class Exercice2_2_0 {
         }
     }
 
+    /**
+     * Méthode pour gérer les commandes du rectangle.
+     * 
+     * @param commande La commande à exécuter.
+     * @param params   Les paramètres de la commande.
+     */
     private void handleRobiCommand(String commande, List<SNode> params) {
         switch (commande) {
             case "setColor":
-            	this.robi.setColor(getColorFromString(params.get(2).contents()));
+                this.robi.setColor(getColorFromString(params.get(2).contents()));
                 break;
             case "translate":
                 int x = Integer.parseInt(params.get(2).contents());
@@ -82,6 +131,12 @@ public class Exercice2_2_0 {
         }
     }
 
+    /**
+     * Méthode pour obtenir une couleur à partir d'une chaîne de caractères.
+     * 
+     * @param colorStr La chaîne de caractères représentant la couleur.
+     * @return La couleur correspondante.
+     */
     private Color getColorFromString(String colorStr) {
         switch (colorStr) {
             case "black":
@@ -97,7 +152,13 @@ public class Exercice2_2_0 {
         }
     }
 
+    /**
+     * Méthode principale, point d'entrée de l'application. Crée une instance de la
+     * classe Exercice2_2_0.
+     * 
+     * @param args Arguments de la ligne de commande (non utilisés).
+     */
     public static void main(String[] args) {
-        new Exercice2_2_0();
+        new Exercice2_2_0(); 
     }
 }
--- Robi2024/Robi/src/exercice4/AddElement.java
+++ Robi2024/Robi/src/exercice4/AddElement.java
@@ -4,36 +4,56 @@ import graphicLayer.GElement;
 import graphicLayer.GSpace;
 import stree.parser.SNode;
 
-
+/**
+ * Commande pour ajouter un élément graphique à l'espace graphique.
+ * 
+ * @author LE BRAS Erwan
+ * @author ROUSVAL Romain
+ * @author NICOLAS Pierre
+ * @author KERVRAN Maxime
+ */
 public class AddElement implements Command {
-    private Environment environment;
+	private Environment environment;
 
-    public AddElement(Environment environment) {
-        this.environment = environment;
-    }
+	/**
+	 * Constructeur de la classe AddElement.
+	 * 
+	 * @param environment L'environnement d'exécution.
+	 */
+	public AddElement(Environment environment) {
+		this.environment = environment;
+	}
 
-    public Reference run(Reference receiver, SNode method) {
-        GSpace space = (GSpace) receiver.getReceiver();
-        String elementType = method.get(3).get(0).contents();
+	/**
+	 * Exécute la commande pour ajouter un élément graphique à l'espace graphique.
+	 * 
+	 * @param receiver Le récepteur de la commande.
+	 * @param method   La méthode à exécuter.
+	 * @return Le récepteur de la commande.
+	 */
+	public Reference run(Reference receiver, SNode method) {
+		GSpace space = (GSpace) receiver.getReceiver();
+		String elementType = method.get(3).get(0).contents();
 
-        Reference refElementClass = this.environment.getReferenceByName(elementType);
+		Reference refElementClass = this.environment.getReferenceByName(elementType);
 
-        try {
-            Command newElementCommand = getNewElementCommand(refElementClass, elementType);
-            if (newElementCommand != null) {
-                Reference newElementReference = newElementCommand.run(refElementClass, method);
-                GElement element = (GElement) newElementReference.getReceiver();
-                this.environment.addReference(method.get(2).contents(), newElementReference);
-                space.addElement(element);
-                return receiver;
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+		try {
+			Command newElementCommand = getNewElementCommand(refElementClass, elementType);
+			if (newElementCommand != null) {
+				Reference newElementReference = newElementCommand.run(refElementClass, method);
+				GElement element = (GElement) newElementReference.getReceiver();
+				this.environment.addReference(method.get(2).contents(), newElementReference);
+				space.addElement(element);
+				return receiver;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-        return null;
-    }
+		return null;
+	}
 
+<<<<<<< HEAD
     public Command getNewElementCommand(Reference refElementClass, String elementType) {
         switch (elementType) {
             case "GRect":
@@ -52,11 +72,31 @@ public class AddElement implements Command {
                 return null;
         }
     }
+=======
+	/**
+	 * Obtient la commande pour créer un nouvel élément graphique.
+	 * 
+	 * @param refElementClass La référence de la classe de l'élément.
+	 * @param elementType     Le type de l'élément.
+	 * @return La commande pour créer un nouvel élément graphique.
+	 */
+	private Command getNewElementCommand(Reference refElementClass, String elementType) {
+		switch (elementType) {
+		case "GRect":
+			return (NewElement) this.environment.getReferenceByName("rect.class").getCommandByName("new");
+		case "GOval":
+			return (NewElement) this.environment.getReferenceByName("oval.class").getCommandByName("new");
+		case "rect.class":
+			return (NewElement) refElementClass.getCommandByName("new");
+		case "oval.class":
+			return (NewElement) refElementClass.getCommandByName("new");
+		case "image.class":
+			return (NewImage) refElementClass.getCommandByName("new");
+		case "label.class":
+			return (NewString) refElementClass.getCommandByName("new");
+		default:
+			return null;
+		}
+	}
+>>>>>>> 1a688b8e99892dffe5cd195b5dbd9c3d807d036c
 }
-
-
-
-
-
-
-
